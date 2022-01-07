2870da164081908a8f30f344771be1d11ec88b27
hwan0806 <rlfghks527@naver.com>
2022-01-07 23:23:34 +0900

    wall_follow implement

--- wall_follow/src/wall_follow.py
+++ wall_follow/src/wall_follow.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import sys
-import math
+from math import *
 import numpy as np
 
 #ROS Imports
@@ -10,9 +10,9 @@ from sensor_msgs.msg import Image, LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 
 #PID CONTROL PARAMS
-kp = #TODO
-kd = #TODO
-ki = #TODO
+kp = 14    #TODO
+kd = 0.09   #TODO
+ki = 0      #TODO
 servo_offset = 0.0
 prev_error = 0.0 
 error = 0.0
@@ -21,9 +21,10 @@ integral = 0.0
 #WALL FOLLOW PARAMS
 ANGLE_RANGE = 270 # Hokuyo 10LX has 270 degrees scan
 DESIRED_DISTANCE_RIGHT = 0.9 # meters
-DESIRED_DISTANCE_LEFT = 0.55
-VELOCITY = 2.00 # meters per second
+DESIRED_DISTANCE_LEFT = 0.8
+VELOCITY = 1.00 # meters per second
 CAR_LENGTH = 0.50 # Traxxas Rally is 20 inches or 0.5 meters
+THETA = pi/10
 
 class WallFollow:
     """ Implement Wall Following on the car
@@ -41,8 +42,15 @@ class WallFollow:
         # angle: between -45 to 225 degrees, where 0 degrees is directly to the right
         # Outputs length in meters to object with angle in lidar scan field of view
         #make sure to take care of nans etc.
+        
+        b = data.ranges[269]
+        
+        i = int(angle / (2*pi/1080))
+        
+        a = data.ranges[269+i]
+        
         #TODO: implement
-        return 0.0
+        return a,b
 
     def pid_control(self, error, velocity):
         global integral
@@ -51,24 +59,41 @@ class WallFollow:
         global ki
         global kd
         angle = 0.0
+        
         #TODO: Use kp, ki & kd to implement a PID controller for 
+        angle = kp * error + kd * (error - prev_error)
+        
+        
         drive_msg = AckermannDriveStamped()
         drive_msg.header.stamp = rospy.Time.now()
         drive_msg.header.frame_id = "laser"
-        drive_msg.drive.steering_angle = angle
+        drive_msg.drive.steering_angle = -angle
         drive_msg.drive.speed = velocity
         self.drive_pub.publish(drive_msg)
+        
+        print(error)
+        prev_error = error
 
     def followLeft(self, data, leftDist):
         #Follow left wall as per the algorithm 
+        a, b = self.getRange(data, THETA)
+        
+        alpha = atan((a*cos(THETA)-b) / a*sin(THETA))
+        
+        D_cur = b*cos(alpha)    # D = distance to wallfrom ego vehicle
+        D_fut = D_cur + CAR_LENGTH*sin(alpha)
+        
+        
         #TODO:implement
-        caculated_error = 0.0
+        caculated_error = D_fut - leftDist
         return caculated_error
 
     def lidar_callback(self, data):
         """ 
         """
         laser_data = data
+        angle = pi/5
+        
         
         error = self.followLeft(laser_data, DESIRED_DISTANCE_LEFT)
         #0.0 #TODO: replace with error returned by followLeft
