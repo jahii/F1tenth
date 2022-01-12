7dc15e1beb2cc3a49e587a4ad2b78bf03982882c
hwan0806 <rlfghks527@naver.com>
2022-01-12 22:20:42 +0900

    str_angle

--- f1tenth_simulator/node/reactive_methods/src/reactive_gap_follow.py
+++ f1tenth_simulator/node/reactive_methods/src/reactive_gap_follow.py
@@ -29,7 +29,7 @@ class reactive_follow_gap:
                 ranges[i]=0.0
         
         ranges = np.array(ranges)
-        ranges = ranges[np.arange(359,719)]
+        ranges = ranges[np.arange(270,809)]
         
         proc_ranges = ranges
         return proc_ranges
@@ -39,33 +39,11 @@ class reactive_follow_gap:
         """ Return the start index & end index of the max gap in free_space_ranges
         """
         free_space_ranges = np.array(free_space_ranges)
+        
+        #for i in range(len(free_space_ranges)):
+            
+        
         max_index = np.where(free_space_ranges==free_space_ranges.max())
-        kp = 10
-        kd = 0.2
-        
-        
-        
-        
-        global prev_error
-        global kp
-        global kd
-        angle = 0.0
-        
-        #TODO: Use kp, ki & kd to implement a PID controller for 
-        angle = kp *  + kd * (error - prev_error)
-        
-        
-        drive_msg = AckermannDriveStamped()
-        drive_msg.header.stamp = rospy.Time.now()
-        drive_msg.header.frame_id = "laser"
-        drive_msg.drive.steering_angle = -angle
-        drive_msg.drive.speed = velocity
-        self.drive_pub.publish(drive_msg)
-        
-        
-        prev_error = error
-        
-        
         
         
         return None
@@ -89,33 +67,57 @@ class reactive_follow_gap:
 
         #Find closest point to LiDAR
         # for i in range(len(proc_ranges)):
-        #     if 
-        #150개의 index를 safety_bubble
-        
+        #     if         
         free_space_ranges=[]
-        min_index = np.where(proc_ranges==proc_ranges.min())
+        
+        min_index_tuple = np.where(proc_ranges==proc_ranges.min())
+        
+        min_index_int = int(min_index_tuple[0])
+        
+        min_range = ranges[min_index_int]               ####################################### error
+        
+        
+        theta = math.atan(1.5/min_range)        # 0.3 = 0.6/2 -> 0.6 = bubble width
+        angle_index = int(theta / data.angle_increment)
+        
+        
+        #Eliminate all points inside 'bubble' (set them to zero) 
         proc_ranges = proc_ranges.tolist()
         for i in range(len(proc_ranges)):
-            if min_index - 75 < i < min_index + 75 : 
+            if min_index_int - angle_index < i < min_index_int + angle_index : 
                 free_space_ranges.append(0.0)
             else :
                 free_space_ranges.append(proc_ranges[i])
                 
-        self.find_max_gap(free_space_ranges)
-    
-        
-        
-
-        #Eliminate all points inside 'bubble' (set them to zero) 
-        
-
+       
         #Find max length gap 
+        max_gap = self.find_max_gap(free_space_ranges)
 
 
         #Find the best point in the gap 
         
+        free_space_ranges = np.array(free_space_ranges)
+        max_index_tuple = np.where(free_space_ranges==free_space_ranges.max())
+        max_index_int = int(max_index_tuple[0])
+        
+        print("max distance :" + str(max_index_int))
+        print("min distance :" + str(min_index_int))
+        print("------------------------------------")
+        
+        angle_diff_index = 270 - max_index_int
+        
+        str_angle = angle_diff_index * data.angle_increment
+        
+        velocity = 1.0
+        
 
         #Publish Drive message
+        drive_msg = AckermannDriveStamped()
+        drive_msg.header.stamp = rospy.Time.now()
+        drive_msg.header.frame_id = "laser"
+        drive_msg.drive.steering_angle = -str_angle
+        drive_msg.drive.speed = velocity
+        self.drive_pub.publish(drive_msg)
         
 
 def main(args):
